8ca574921a77e653128443d40599bc78634e4326
Andyjk1406 <32988487+Andyjk1406@users.noreply.github.com>
2018-07-11 15:26:27 +0100

    Another commit

--- YodaOcclusion.cpp
+++ YodaOcclusion.cpp
@@ -1,3 +1,8 @@
+/**
+* File created by Andrew Keeling, University of Leeds
+* Measure Occlusion. For research in conjunction with Tim Yoda, Basel, Switzerland
+*/
+
 #include <vtkVersion.h>
 #include <vtkSmartPointer.h>
  
@@ -7,178 +12,113 @@
 #include <vtkRenderer.h>
 #include <vtkRenderWindow.h>
 #include <vtkRenderWindowInteractor.h>
-#include <vtkPLYReader.h>
+#include <vtkSTLReader.h>
 #include <vtkCleanPolyData.h>
 #include <vtkProperty.h>
 #include <vtkPointData.h>
 #include <vtkScalarBarActor.h>
-#include <vtkSphereSource.h>
-#include <vtkBooleanOperationPolyDataFilter.h>
-#include <vtkMatrix4x4.h>
-#include <vtkTransform.h>
-#include <vtkTransformPolyDataFilter.h>
+#include <vtkXMLPolyDataWriter.h>
+#include <vtkXMLPolyDataReader.h>
+#include <vtkPLYReader.h>
+#include <vtkPLYWriter.h>
+#include <vtkDataArray.h>
+#include <vtkDoubleArray.h>
 
-#include <pcl/io/pcd_io.h>
-#include <pcl/io/vtk_lib_io.h>
-#include <pcl/registration/gicp.h>
-
-typedef pcl::PointNormal PointT;
-typedef pcl::PointCloud<PointT>  PointCloudT;
 
 float standard_deviation(vtkSmartPointer<vtkDistancePolyDataFilter> dist);
-void getWorstFraction(vtkSmartPointer<vtkDistancePolyDataFilter> dist, float fraction);
-
-bool saveALNFile(std::string filename, std::vector<std::string>& v_meshNames, std::vector<Eigen::Matrix4f> v_mats);
 
 bool fileExists(const char *fileName);
-
-std::string src_filename, tgt_filename, base_dir;
  
 int main(int argc, char* argv[])
 {
   	
-  vtkSmartPointer<vtkPolyData> source;
-  vtkSmartPointer<vtkPolyData> target;
-  vtkSmartPointer<vtkPolyData> target_cropped;
-  float fraction;
+  vtkSmartPointer<vtkPolyData> upper;
+  vtkSmartPointer<vtkPolyData> lower;
 
-  // Load the 3 PLY files, source, target, target_cropped
-  if (argc == 6)
-    {
+  std::string upper_filename, lower_filename;
+  std::string upper_filename_stl, lower_filename_stl;
+  std::string upper_filename_vtp, lower_filename_vtp;
+ 
+  // Load the 2 STL files, upper and lower
+  if (argc == 3)
+   {
 
-	base_dir = argv[1];
-	std::string temp;
+	 upper_filename = argv[1];
+	 lower_filename = argv[2];
 
-	vtkSmartPointer<vtkPLYReader> reader1 = vtkSmartPointer<vtkPLYReader>::New();
-	temp = base_dir;
-	temp +=argv[2];
-	reader1->SetFileName(temp.c_str());
+	 upper_filename_stl = upper_filename + ".stl";
+	 lower_filename_stl = lower_filename + ".stl";
+
+	 upper_filename_vtp = upper_filename + ".vtp";
+	 lower_filename_vtp = lower_filename + ".vtp";
+
+	vtkSmartPointer<vtkSTLReader> reader1 = vtkSmartPointer<vtkSTLReader>::New();
+	reader1->SetFileName(upper_filename_stl.c_str());
 	reader1->Update();
-	source = reader1->GetOutput();	
+	upper = reader1->GetOutput();	
 
-	vtkSmartPointer<vtkPLYReader> reader2 = vtkSmartPointer<vtkPLYReader>::New();
-	temp = base_dir;
-	temp += argv[3];
-	reader2->SetFileName(temp.c_str());
+	vtkSmartPointer<vtkSTLReader> reader2 = vtkSmartPointer<vtkSTLReader>::New();
+	reader2->SetFileName(lower_filename_stl.c_str());
 	reader2->Update();
-	target = reader2->GetOutput();		
-
-	vtkSmartPointer<vtkPLYReader> reader3 = vtkSmartPointer<vtkPLYReader>::New();
-	temp = base_dir;
-	temp += argv[4];
-	reader3->SetFileName(temp.c_str());
-	reader3->Update();
-	target_cropped = reader3->GetOutput();
-
-	fraction = atof(argv[5]);
-	
-	src_filename = argv[2];
-	tgt_filename = argv[4];
-
+	lower = reader2->GetOutput();		
+		
     }
   else
     {
-	  cout << "Usage : ...exe path/to/dir/ source.ply target.ply target_cropped.ply fraction";
+	  cout << "Usage : ...exe upper(.stl) lower(.stl) [The '.stl' will be appended automatically]";
 	  return 0;
   }
 
-  // Create PCL clouds for alignment (source and target)
-  PointCloudT::Ptr source_cloud(new PointCloudT);
-  pcl::io::vtkPolyDataToPointCloud(source, *source_cloud);
- 
-  PointCloudT::Ptr target_cloud(new PointCloudT);
-  pcl::io::vtkPolyDataToPointCloud(target, *target_cloud);
-
-  // Align source to target using GICP
-  Eigen::Matrix4f icpMatrix;
-  PointCloudT::Ptr icp_alignedCloud(new PointCloudT);
-
-  pcl::GeneralizedIterativeClosestPoint<PointT, PointT> icp;
-  icp.setMaximumIterations(100);
-  icp.setTransformationEpsilon(1e-12);
-  icp.setEuclideanFitnessEpsilon(1e-12);
-  icp.setInputSource(source_cloud);
-  icp.setInputTarget(target_cloud);
-
-  icp.setMaxCorrespondenceDistance(2); // Start with 2mm search zone
-  icp.align(*icp_alignedCloud);
-  icpMatrix = icp.getFinalTransformation();
-
-  icp.setMaxCorrespondenceDistance(0.3); // Reduce to 0.3mm search zone
-  icp.align(*icp_alignedCloud, icpMatrix);
-  icpMatrix = icp.getFinalTransformation();
-
-  // Convert the transformation into a vtk readable form
-  vtkSmartPointer<vtkMatrix4x4> VTKmatrix = vtkSmartPointer<vtkMatrix4x4>::New();
-  for (unsigned int r = 0; r<4; r++) {
-	  for (unsigned int c = 0; c<4; c++)
-		  VTKmatrix->Element[r][c] = icpMatrix(r, c);
-  }
-
-  // Transform the source PLY
-  vtkSmartPointer<vtkTransform> transform = vtkSmartPointer<vtkTransform>::New();
-  transform->SetMatrix(VTKmatrix);
-
-  vtkSmartPointer<vtkTransformPolyDataFilter> filter = vtkSmartPointer<vtkTransformPolyDataFilter>::New();
-  filter->SetTransform(transform);
-  filter->SetInputData(source);
-  filter->Update();
-
- // Clean the transformed source and the cropped target (might need this for the vtkDistanceFilter?)
-  vtkSmartPointer<vtkCleanPolyData> cleanSource = vtkSmartPointer<vtkCleanPolyData>::New();
-  cleanSource->SetInputData( filter->GetOutput());
- 
-  vtkSmartPointer<vtkCleanPolyData> cleanTarget_cropped =vtkSmartPointer<vtkCleanPolyData>::New();
-  cleanTarget_cropped->SetInputData( target_cropped);
-
- 
   // Calculate the distance from target_crop to source
   vtkSmartPointer<vtkDistancePolyDataFilter> distanceFilter = vtkSmartPointer<vtkDistancePolyDataFilter>::New();
-  distanceFilter->SetInputConnection( 0, cleanTarget_cropped->GetOutputPort() );
-  distanceFilter->SetInputConnection( 1, cleanSource->GetOutputPort() );
+  distanceFilter->SetInputData( 0, upper );
+  distanceFilter->SetInputData( 1, lower );
+  distanceFilter->ComputeSecondDistanceOn();
   distanceFilter->Update();
 
-/*
-  // Boolean (Not used in the distance code)
-  vtkSmartPointer<vtkBooleanOperationPolyDataFilter> booleanOp = vtkSmartPointer<vtkBooleanOperationPolyDataFilter>::New();
-  booleanOp->SetOperationToIntersection();
-  booleanOp->SetInputConnection(0, clean1->GetOutputPort());
-  booleanOp->SetInputConnection(1, clean2->GetOutputPort());
-
-  vtkSmartPointer<vtkPolyDataMapper> bmapper = vtkSmartPointer<vtkPolyDataMapper>::New();
-  bmapper->SetInputConnection(booleanOp->GetOutputPort());
-  bmapper->ScalarVisibilityOff();
-  vtkSmartPointer<vtkActor> bactor =vtkSmartPointer<vtkActor>::New();
-  bactor->SetMapper(bmapper);
-*/
-
   // Get the mean/SD data and save it
-  float sd = standard_deviation(distanceFilter);
+  //float sd = standard_deviation(distanceFilter);
 
-  // Get the worst xx% and save the data
-  getWorstFraction(distanceFilter, fraction);
+  // Save the upper with the distance scalars as a vtp file
+  vtkNew<vtkXMLPolyDataWriter> writer;
+  writer->SetFileName(upper_filename_vtp.c_str());
+  writer->SetInputConnection(distanceFilter->GetOutputPort(0));
+  writer->Write();
+  writer->SetFileName(lower_filename_vtp.c_str());
+  writer->SetInputConnection(distanceFilter->GetOutputPort(1));
+  writer->Update();
+  writer->Write();
 
-  // Save a meshlab ALN file--------------------------------------------------------------------------------------------
-  std::string aln_filename = base_dir + src_filename.erase(src_filename.length() - 4) + "_" + tgt_filename.erase(tgt_filename.length() - 4)+".aln";
-  std::vector<std::string> v_meshNames;
-  std::vector<Eigen::Matrix4f> v_mats;
+/*  
+  vtkNew<vtkPolyData> orig;
+  vtkNew<vtkPolyData> loaded;
 
-  v_meshNames.push_back(src_filename); // Now with no '.ply'
-  v_meshNames.push_back(tgt_filename); // Now with no '.ply'
+  orig->DeepCopy(distanceFilter->GetOutput());
 
-  v_mats.push_back(icpMatrix);
-  v_mats.push_back(Eigen::Matrix4f::Identity());
+  // Load the upper with the distance scalars as a vtp file
+  vtkNew<vtkXMLPolyDataReader> reader;
+  reader->SetFileName(lower_filename_vtp.c_str());
+  reader->Update();
+  loaded->DeepCopy(reader->GetOutput());
 
-  saveALNFile(aln_filename, v_meshNames, v_mats);
-  //----------------------------------------------------------------------------------------------------------------------
+  // Check Scalars
+  vtkSmartPointer<vtkDoubleArray> orig_scalars;
+  vtkSmartPointer<vtkDoubleArray> loaded_scalars;
 
+  orig_scalars = vtkDoubleArray::SafeDownCast(orig->GetPointData()->GetScalars());
+  loaded_scalars = vtkDoubleArray::SafeDownCast(loaded->GetPointData()->GetScalars());
+
+  std::cout << "Original : " << orig_scalars->GetTuple1(10) << std::endl;
+  std::cout << "Loaded : " << loaded_scalars->GetTuple1(10) << std::endl;
+
+  */
  
   // Display colour mapped image if we want
 /*
   vtkSmartPointer<vtkPolyDataMapper> mapper = vtkSmartPointer<vtkPolyDataMapper>::New();
-  mapper->SetInputConnection( distanceFilter->GetOutputPort() );
-  mapper->SetScalarRange(distanceFilter->GetOutput()->GetPointData()->GetScalars()->GetRange()[0],
-   distanceFilter->GetOutput()->GetPointData()->GetScalars()->GetRange()[1]);
+  mapper->SetInputConnection( distanceFilter->GetOutputPort(1) );
+  mapper->SetScalarRange(distanceFilter->GetOutput(1)->GetPointData()->GetScalars()->GetRange()[0],
+   distanceFilter->GetOutput(1)->GetPointData()->GetScalars()->GetRange()[1]);
  
   vtkSmartPointer<vtkActor> actor = vtkSmartPointer<vtkActor>::New();
   actor->SetMapper( mapper );
@@ -186,7 +126,7 @@ int main(int argc, char* argv[])
   vtkSmartPointer<vtkScalarBarActor> scalarBar =  vtkSmartPointer<vtkScalarBarActor>::New();
   scalarBar->SetLookupTable(mapper->GetLookupTable());
   scalarBar->SetTitle("Distance");
-  scalarBar->SetNumberOfLabels(4);
+  scalarBar->SetNumberOfLabels(10);
   vtkSmartPointer<vtkRenderer> renderer = vtkSmartPointer<vtkRenderer>::New();
  
   vtkSmartPointer<vtkRenderWindow> renWin = vtkSmartPointer<vtkRenderWindow>::New();
@@ -202,37 +142,10 @@ int main(int argc, char* argv[])
   renWinInteractor->Start();
  
  */
+
   return EXIT_SUCCESS;
 }
 
-void getWorstFraction(vtkSmartPointer<vtkDistancePolyDataFilter> dist, float fraction)
-{
-
-	vtkIdType nValues;
-	nValues = dist->GetOutput()->GetPointData()->GetScalars()->GetSize();
-	std::vector<float> vDistances(nValues);
-
-	for (vtkIdType i = 0; i<nValues; ++i)
-	{
-		double val = dist->GetOutput()->GetPointData()->GetScalars()->GetTuple1(i);
-		vDistances[i] = fabs(val);
-	}
-
-	std::sort(vDistances.begin(), vDistances.end(), std::greater<float>());
-
-	int nDistsToSave =(int) ((float)nValues * fraction);
-
-	ofstream out("WorstPercentage.txt", ios::app);
-	out << src_filename << " " << tgt_filename <<" ";
-
-	for (int i = 0; i < nDistsToSave; i++)
-		out << vDistances[i] << " ";
-
-	out << std::endl;
-
-	out.close();
-
-}
 
 float standard_deviation(vtkSmartPointer<vtkDistancePolyDataFilter> dist)
 {
@@ -279,6 +192,7 @@ float standard_deviation(vtkSmartPointer<vtkDistancePolyDataFilter> dist)
 	sum_deviationNeg = sqrt(sum_deviationNeg / negCtr);
 	sum_deviationPos = sqrt(sum_deviationPos / posCtr);
 
+	/*
 	ofstream out;
 	if (!fileExists("DataResults.txt"))
 	{
@@ -301,6 +215,7 @@ float standard_deviation(vtkSmartPointer<vtkDistancePolyDataFilter> dist)
 	out << " " << dist->GetOutput()->GetPointData()->GetScalars()->GetRange()[0] << " " << dist->GetOutput()->GetPointData()->GetScalars()->GetRange()[1] << std::endl;
 
 	out.close();
+	*/
 
 
 	std::cout << "Mean " << mean;
@@ -322,38 +237,4 @@ bool fileExists(const char *fileName)
 	return infile.good();
 }
 
-bool saveALNFile(std::string filename, std::vector<std::string>& v_meshNames, std::vector<Eigen::Matrix4f> v_mats) {
 
-	Eigen::Matrix4f trans_mat;
-
-	ofstream myfile;  // The write stream
-	myfile.open(filename, std::ios_base::out);  // Open the file
-
-	if (!myfile) { return false; } // File opening failed 
-
-	else {
-		myfile << v_mats.size() << std::endl;
-
-		for (int i = 0; i < v_mats.size(); i++) {
-
-			//boost::filesystem::path p(v_meshNames[i]);
-			//std::cout << "filename and extension : " << p.filename() << std::endl; // file.ext
-			//std::cout << "filename only          : " << p.stem() << std::endl;     // file
-			//std::string stem = p.stem().string(); // filename only
-			std::string name = v_meshNames[i];
-
-			myfile << name + ".ply" << std::endl;
-			myfile << "#" << std::endl;
-
-			for (int row = 0; row < 4; row++) {
-				myfile << v_mats[i](row, 0) << " " << v_mats[i](row, 1) << " " << v_mats[i](row, 2) << " " << v_mats[i](row, 3) << std::endl;
-			}
-
-		}
-
-		myfile << "0" << std::endl;
-	}
-
-	return true;
-
-}
